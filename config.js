d10ffbd48574c119fd6fe8457704c19744d58e0e
Donald Spears <paul.spears@oasisdigital.com>
2015-08-18 21:09:40 -0500

    Fix Alpha 34 update, fix broken typescript upgrade

--- config.js
+++ config.js
@@ -1,33 +1,38 @@
 System.config({
-  "baseURL": "/",
-  "defaultJSExtensions": true,
-  "transpiler": "typescript",
-  "typescriptOptions": {
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "typescript",
+  typescriptOptions: {
     "module": "commonjs",
     "emitDecoratorMetadata": true
   },
-  "paths": {
+  paths: {
     "github:*": "jspm_packages/github/*",
     "app": "app",
     "npm:*": "jspm_packages/npm/*"
   },
-  "packages": {
+
+  packages: {
     "app": {
       "main": "app",
       "defaultExtension": "ts"
     }
-  }
-});
+  },
 
-System.config({
-  "map": {
+  map: {
     "angular2": "npm:angular2@2.0.0-alpha.34",
     "es6-shim": "github:es-shims/es6-shim@0.33.0",
     "reflect-metadata": "npm:reflect-metadata@0.1.0",
-    "typescript": "github:mhegazy/typescript@v1.5-beta2",
+    "typescript": "npm:typescript@1.5.3",
     "zone.js": "npm:zone.js@0.5.2",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-buffer@0.1.0": {
+      "buffer": "npm:buffer@3.4.2"
+    },
+    "github:jspm/nodelibs-os@0.1.0": {
+      "os-browserify": "npm:os-browserify@0.1.2"
     },
     "github:jspm/nodelibs-path@0.1.0": {
       "path-browserify": "npm:path-browserify@0.0.0"
@@ -53,8 +58,16 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
+    "npm:buffer@3.4.2": {
+      "base64-js": "npm:base64-js@0.0.8",
+      "ieee754": "npm:ieee754@1.1.6",
+      "is-array": "npm:is-array@1.0.1"
+    },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:os-browserify@0.1.2": {
+      "os": "github:jspm/nodelibs-os@0.1.0"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
@@ -67,6 +80,15 @@ System.config({
     },
     "npm:rx@2.5.1": {
       "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:typescript@1.5.3": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "os": "github:jspm/nodelibs-os@0.1.0",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "readline": "github:jspm/nodelibs-readline@0.1.0"
     },
     "npm:url@0.10.3": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
@@ -83,4 +105,3 @@ System.config({
     }
   }
 });
-
